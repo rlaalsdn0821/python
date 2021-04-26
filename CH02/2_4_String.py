8cde5301cbcc602a1576bd87f1f4c031a950658f
rlaalsdn0821 <kimminwoo0821@naver.com>
2021-04-26 17:55:46 +0900

    2021/04/26 파이썬 CH02 수업예제

--- CH02/2_4_String.py
+++ CH02/2_4_String.py
@@ -4,3 +4,15 @@
 내용: 파이썬 변수외 자료형 교제 p.38
 """
 
+#문자열 더하기
+str1 ='Hello'
+str2 ='Python'
+str3 =str1+str2
+print('srt3:', str3)
+
+#문자열 곱하기
+name='홍길동'
+print('name * 3:', name * 3)
+
+msg='Hello World'
+print('msg 길이:', len(msg))   #len: 길이
