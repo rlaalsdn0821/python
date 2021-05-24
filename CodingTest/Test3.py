5d1db33fc51fa667178152c36596422090cdf2ee
rlaalsdn0821 <kimminwoo0821@naver.com>
2021-05-24 17:20:17 +0900

    2021/05/24 코딩 테스트 추가

---
+++ CodingTest/Test3.py
@@ -0,0 +1,22 @@
+"""
+날짜: 2021/05/24
+이름: 김민우
+내용: 코딩 테스트 - 1이 될 때까지
+"""
+
+n, k = map(int, input().split())
+result = 0
+
+while True:
+
+    if n == 1:
+        break
+
+    if n % k == 0:
+        n /= k
+    else:
+        n -= 1
+
+    result += 1
+
+print(result)
